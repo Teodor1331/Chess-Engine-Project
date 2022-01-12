6aa93abc6cdbd0c4bbd6120392eacc3bbedc28df
Teodor1331 <teodor.sdishanski@gmail.com>
2022-01-12 02:06:33 +0200

    Add Randomized Pieces Movement

--- ChessModule/GameEngine.py
+++ ChessModule/GameEngine.py
@@ -1,3 +1,7 @@
+from GameHelpers import build_ranks_to_rows_notation
+from GameHelpers import build_files_to_columns_notation
+
+
 class GameState:
     def __init__(self):
         self.__board        =   self.generate_board()
@@ -26,6 +30,46 @@ class GameState:
         return self.__game_moves
 
 
+    @board.setter
+    def board(self, board):
+        self.__board = board
+
+
+    @whites_turn.setter
+    def whites_turn(self, whites_turn):
+        self.__whites_turn = whites_turn
+
+
+    @blacks_turn.setter
+    def blacks_turn(self, blacks_turn):
+        self.__blacks_turn = blacks_turn
+
+
+    @game_moves.setter
+    def game_moves(self, game_moves):
+        self.__game_moves = game_moves
+
+
+    @board.deleter
+    def board(self):
+        del self.__board
+
+
+    @whites_turn.deleter
+    def whites_turn(self):
+        del self.__whites_turn
+
+
+    @blacks_turn.deleter
+    def blacks_turn(self):
+        del self.__blacks_turn
+
+
+    @game_moves.deleter
+    def game_moves(self):
+        del self.__game_moves
+
+
     def __del__(self):
         del self.__board
         del self.__whites_turn
@@ -44,3 +88,90 @@ class GameState:
             ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
             ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
         ]
+
+    
+    def change_board_place(self, row, col, value):
+        self.__board[row][col] = value
+
+
+    def add_game_move(self, move):
+        assert isinstance(move, Move)
+        self.__game_moves.append(move)
+
+
+    def make_move(self, move):
+        assert isinstance(move, Move)
+
+        if self.board[move.start_row][move.start_col] != '.':
+            self.change_board_place(move.start_row, move.start_col, '.')
+            self.change_board_place(move.end_row, move.end_col, move.moved_piece)
+            self.add_game_move(move)
+            self.whites_turn = not self.whites_turn
+            self.blacks_turn = not self.blacks_turn
+
+    
+class Move:
+    RANKS_TO_ROWS       = build_ranks_to_rows_notation()
+    FILES_TO_COLUMNS    = build_files_to_columns_notation()
+    ROWS_TO_RANKS       = {value: key for key, value in RANKS_TO_ROWS.items()}
+    COLUMNS_TO_FILES    = {value: key for key, value in FILES_TO_COLUMNS.items()}
+
+    def __init__(self, start, end, board):
+        self.__start_row    =   start[0]
+        self.__start_col    =   start[1]
+
+        self.__end_row      =   end[0]
+        self.__end_col      =   end[1]
+
+        self.__moved_piece  =   board[start[0]][start[1]]
+        self.__taken_piece  =   board[end[0]][end[1]]
+
+
+    @property
+    def start_row(self):
+        return self.__start_row
+
+
+    @property
+    def start_col(self):
+        return self.__start_col
+
+
+    @property
+    def end_row(self):
+        return self.__end_row
+
+
+    @property
+    def end_col(self):
+        return self.__end_col
+
+
+    @property
+    def moved_piece(self):
+        return self.__moved_piece
+
+
+    @property
+    def taken_piece(self):
+        return self.__taken_piece
+
+
+    def __del__(self):
+        del self.__start_row
+        del self.__start_col
+
+        del self.__end_row
+        del self.__end_col
+
+        del self.__moved_piece
+        del self.__taken_piece
+
+
+    def __repr__(self):
+        return  self.get_rank_file(self.start_row, self.start_col) + \
+                self.get_rank_file(self.end_row, self.end_col)
+ 
+
+    def get_rank_file(self, row, column):
+        return self.COLUMNS_TO_FILES[column] + self.ROWS_TO_RANKS[row]
--- ChessModule/GameMain.py
+++ ChessModule/GameMain.py
@@ -1,5 +1,8 @@
 from GameEngine     import GameState
-from GameGraphics   import *
+from GameEngine     import Move
+
+from GameGraphics   import load_pieces
+from GameGraphics   import draw_game_state
 
 import pygame
 
@@ -15,10 +18,33 @@ def main():
     load_pieces()
     running = True
 
+    square_selected     = tuple()
+    player_clicks       = list()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                location = pygame.mouse.get_pos()
+
+                j = location[0] // 64
+                i = location[1] // 64
+
+                if square_selected == (i, j):
+                    square_selected = tuple()
+                    player_clicks   = list()
+                else:
+                    square_selected = (i, j)
+                    player_clicks.append(square_selected)
+
+                if len(player_clicks) == 2:
+                    move = Move(player_clicks[0], player_clicks[1], game_state.board)
+                    game_state.make_move(move)
+
+                    square_selected = tuple()
+                    player_clicks   = list()
+
         draw_game_state(screen, game_state)
         clock.tick(15)
         pygame.display.flip()
